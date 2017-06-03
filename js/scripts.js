c04d322d82b9c7dd23c1684678a0ddc9b6533445
vturcios90 <vturcios90@icloud.com>
2017-06-03 04:32:22 -0400

    Added query  and js script for fade in effects on page load.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("body").ready(function(){
+        $("body").hide();
+        $("body").fadeIn(2000);
+    });
+    // $(".btn2").click(function(){
+    //     $("p").show(1000);
+    // });
+});
